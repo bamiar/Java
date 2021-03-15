ff11a82ec7896231704047c33f56043c8758b4e2
bamiar <ginea00@gmail.com>
2021-03-15 16:59:24 +0900

    https://www.hackerrank.com/challenges/java-bitset/problem

---
+++ Solution.java
@@ -0,0 +1,42 @@
+import java.util.BitSet;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int size = scanner.nextInt();
+        int cal = scanner.nextInt();
+
+        BitSet B1 = new BitSet(size);
+        BitSet B2 = new BitSet(size);
+        BitSet[] bitsets = new BitSet[]{B1, B2};
+
+        while(0 < cal--) {
+            final String operation = scanner.next();
+            final int a = scanner.nextInt()-1;
+            final int b = scanner.nextInt();
+
+            switch (operation) {
+                case "AND" :
+                    bitsets[a].and(bitsets[b-1]);
+                    break;
+                case "OR" :
+                    bitsets[a].or(bitsets[b-1]);
+                    break;
+                case "XOR" :
+                    bitsets[a].xor(bitsets[b-1]);
+                    break;
+                case "FLIP" :
+                    bitsets[a].flip(b);
+                    break;
+                case "SET" :
+                    bitsets[a].set(b);
+                    break;
+                default:
+                    System.out.println("no proper operation.");
+            }
+            System.out.printf("%d %d%n", bitsets[0].cardinality(), bitsets[1].cardinality());
+        }
+
+    }
+}
