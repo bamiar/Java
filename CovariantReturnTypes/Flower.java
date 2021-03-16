0d9626c2883ff4172f3e8e4a6e40047b8a58d37c
bamiar <ginea00@gmail.com>
2021-03-16 13:48:15 +0900

    https://www.hackerran.com/challenges/java-covariance/problem

---
+++ CovariantReturnTypes/Flower.java
@@ -0,0 +1,47 @@
+package CovariantReturnTypes;
+
+//Complete the classes below
+class Flower {
+    public String whatsYourName() {
+        return "I have many names and types.";
+    }
+}
+
+class Jasmine extends Flower{
+    @Override
+    public String whatsYourName() {
+        final String name = "Jasmine";
+        return name;
+    }
+}
+
+class Lily extends Flower {
+    @Override
+    public String whatsYourName() {
+        final String name = "Lily";
+        return name;
+    }
+}
+
+class Region {
+    public Flower yourNationalFlower() {
+        Flower flower = new Flower();
+        return flower;
+    }
+}
+
+class WestBengal extends Region{
+    @Override
+    public Flower yourNationalFlower() {
+        Jasmine jasmine = new Jasmine();
+        return jasmine;
+    }
+}
+
+class AndhraPradesh extends Region{
+    @Override
+    public Flower yourNationalFlower() {
+        Lily lily  = new Lily();
+        return lily;
+    }
+}
