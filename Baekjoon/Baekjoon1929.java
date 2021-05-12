c049a97caeb9cf622b552e265a1243597fab8c21
bamiar <ginea00@gmail.com>
2021-05-12 22:56:57 +0900

    20210606

---
+++ Baekjoon/Baekjoon1929.java
@@ -0,0 +1,30 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon1929 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        boolean[] e = new boolean[n+1];
+        e[0] = e[1] = true;
+
+        //체 알고리즘
+      for(int i=2; i<=Math.sqrt(n); i++) {
+          if(e[i]) {continue;}
+          for (int j=i+i; j<=n; j+=i) {
+              e[j] = true;
+          }
+      }
+
+        for (int i=m; i<=n; i++) {
+            if (!e[i]) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+}
