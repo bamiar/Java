e1f04953db529fb4d6b2005232722e04a18bc48f
bamiar <ginea00@gmail.com>
2021-05-20 22:27:04 +0900

    20210520

---
+++ Baekjoon/Baekjoon10844.java
@@ -0,0 +1,32 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon10844 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[][] d = new long[n+1][10];
+
+        for (int i=1; i<=9; i++) {
+            d[1][i] = 1;
+        }
+
+        for (int i=2; i<=n; i++) {
+            for (int j=0; j<=9; j++) {
+                if (j==0) {
+                    d[i][j] = (d[i-1][j+1]) % 1000000000;
+                } else if(j==9) {
+                    d[i][j] = (d[i-1][j-1]) % 1000000000;
+                } else
+                d[i][j] = (d[i-1][j-1] + d[i-1][j+1]) % 1000000000;
+            }
+        }
+
+        long result =0;
+        for(int i=0; i<=9; i++) {
+            result += d[n][i];
+        }
+        System.out.println(result % 1000000000);
+    }
+}
