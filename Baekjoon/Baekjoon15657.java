f7eba7231411ff520f75f371d342e8c962179e7e
bamiar <ginea00@gmail.com>
2021-06-04 14:13:32 +0900

    20210603

--- Baekjoon/Baekjoon15657.java
+++ Baekjoon/Baekjoon15657.java
@@ -1,9 +1,10 @@
 package Baekjoon;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Baekjoon15656 {
+public class Baekjoon15657 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static int[] sortedArr; //input 오름차순으로 정렬
@@ -29,48 +30,36 @@ public class Baekjoon15656 {
         for(int i=n-1; i>=0; i--) {
             count[input[i]]++;
         }
-
         for(int i=1; i<count.length; i++) {
             count[i] +=count[i-1];
         }
-
         for(int i=n-1; i>=0; i--) {
             sortedArr[count[input[i]]-1] = input[i];
         }
 
         //재귀함수 call
-        go(0, n, m);
+        go(0, 0, n, m);
 
         System.out.println(sb);
-//        br.close();
-//        bw.close();
     }
 
 
-    static void go(int index, int n, int m) {
+    static void go(int index, int start, int n, int m) {
         //index : 값을 채워넣을 result 수열 위치
         //n 자연수 개수
         //m 수열 길이
         if(index ==m) {
             //result 출력
-//            try {
-                for(int i=0; i<m; i++) {
-//                    bw.write(result[i] + " ");
-                    sb.append(result[i] + " ");
-                }
-//                bw.write("\n"); // 이런식으로 매번 출력해주게되면 시간초과가 난다.
-//                bw.flush(); //출력문은 재귀 밖으로 뺄 것ㅠ
-                sb.append("\n");
-                return;
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            return;
+            for(int i=0; i<m; i++) {
+                sb.append(result[i] + " ");
+            }
+            sb.append("\n");
+            return;
         }
 
-        for(int i=0; i<n; i++) {
+        for(int i=start; i<n; i++) {
             result[index] = sortedArr[i];
-            go(index+1, n, m);
+            go(index+1, i, n, m);
         }
     }
 }
