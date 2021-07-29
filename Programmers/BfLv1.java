b1abbd729be0288536bc1ba54f95b444e56bbcf3
bamiar <ginea00@gmail.com>
2021-07-30 00:12:27 +0900

    20210729

---
+++ Programmers/BfLv1.java
@@ -0,0 +1,41 @@
+package Programmers;
+
+import java.util.ArrayList;
+
+public class BfLv1 {
+    static int[] p1 = {1,2,3,4,5};
+    static int[] p2 = {2,1,2,3,2,4,2,5};
+    static int[] p3 = {3,3,1,1,2,2,4,4,5,5};
+
+    public static void main(String[] args) {
+        int[] input = {1,2,3,4,5};
+    }
+
+    public static int[] solution(int[] answers) {
+        int[] answer;
+        int[] count = new int[3];
+
+        for(int i=0; i<answers.length; i++) {
+            if(answers[i] == p1[i%5]) {count[0]++;}
+            if(answers[i] == p2[i%8]) {count[1]++;}
+            if(answers[i] == p3[i%10]) {count[2]++;}
+        }
+
+        int maxCount = Math.max(Math.max(count[0], count[1]), count[2]);
+        ArrayList<Integer> result = new ArrayList();
+
+        for(int i=0; i<count.length; i++) {
+            if (count[i]==maxCount) {
+                result.add(i);
+            }
+        }
+
+        answer = new int[result.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = result.get(i);
+        }
+
+        return answer;
+    }
+
+}
---
+++ Programmers/HashLv2.java
@@ -0,0 +1,27 @@
+package Programmers;
+
+import java.util.HashMap;
+
+public class HashLv2 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+//        Arrays.sort(phone_book);
+
+        for(int i=0; i<phone_book.length; i++) {
+            hashMap.put(phone_book[i],i);
+        }
+
+        for(int i=0; i<hashMap.size(); i++) {
+            for(int j = 1; j < phone_book[i].length(); j++) {
+                if(hashMap.containsKey(phone_book[i].substring(0,j))) {
+                    answer = false;
+                    return answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
