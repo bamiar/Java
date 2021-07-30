5c4fc30d1abd07feae647138715829adc96188b9
bamiar <ginea00@gmail.com>
2021-07-30 17:28:03 +0900

    20210730

---
+++ Programmers/SortingLv1.java
@@ -0,0 +1,36 @@
+package Programmers;
+
+
+import java.util.Arrays;
+
+class SortingLv1 {//k번째수
+
+    public static void main(String[] args) {
+        solution(new int[] {1, 5, 2, 6, 3, 7, 4}, new int[][] {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}});
+    }
+
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++) {
+            int start = commands[i][0]-1;
+            int end = commands[i][1]-1;
+            int target = commands[i][2]-1;
+            int index = 0;
+
+            int[] temp = new int[end-start+1];
+
+            for(int j=start; j<=end; j++) {
+                temp[index] = array[j];
+                index++;
+            }
+
+            Arrays.sort(temp);
+
+            answer[i] = temp[target];
+        }
+
+        return answer;
+    }
+
+    }
---
+++ Programmers/SortingLv2.java
@@ -0,0 +1,29 @@
+package Programmers;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SortingLv2 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {3, 0, 6, 1, 5}));
+    }
+
+    public static int solution(int[] citations) { //H-Index 틀림 ㅠ
+        int answer = 0;
+
+        Integer[] temp = Arrays.stream(citations).boxed().toArray(Integer[]::new);
+        Arrays.sort(temp, Collections.reverseOrder());
+
+        for(int i=0; i<temp.length; i++) {
+            if(i+1<temp.length) {
+                if(i+1>=temp[i] && temp.length-i<=i) {
+                    answer = i;
+                }
+            }
+         }
+
+        return answer;
+    }
+
+}
