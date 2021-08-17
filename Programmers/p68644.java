24d7aa58ce4e8380f23fdfa2f360c53fe1f1f484
bamiar <ginea00@gmail.com>
2021-08-17 23:36:19 +0900

    20210817

---
+++ Programmers/p68644.java
@@ -0,0 +1,31 @@
+package Programmers;
+
+import java.util.*;
+
+public class p68644 {
+    public static void main(String[] a) {
+        solution(new int[] {2,1,3,4,1});
+    }
+
+    public static int[] solution(int[] numbers) {
+        HashSet<Integer> sum = new HashSet();
+
+        for(int i=0; i< numbers.length; i++) {
+            for(int j=i; j<numbers.length; j++) {
+                if(i==j) continue;
+                sum.add(numbers[i]+ numbers[j]);
+            }
+        }
+
+        List<Integer> result = new ArrayList<>(sum);
+        Collections.sort(result);
+
+        int[] answer = new int[result.size()];
+        for(int i=0; i<result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+
+        return answer;
+    }
+
+}
