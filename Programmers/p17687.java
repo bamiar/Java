7d4645ca45e0e252ddeb07715587d30964c52841
bamiar <ginea00@gmail.com>
2021-10-25 23:43:46 +0900

    20211025

--- Programmers/p17687.java
+++ Programmers/p17687.java
@@ -1,8 +1,28 @@
 package Programmers;
 
-public class p17687_1 {
+public class p17687 {
     public static String solution(int n, int t, int m, int p) {
-        //n=진법 t=튜브가 말해야하는 숫자들 m=게임인원 p=튜브의 순서
+        //2차풀이, 1차가 효율은 더 좋다
+//        String answer = "";
+//
+//        int temp = 0;
+//        while(answer.length()<=(t-1)*m +p) {
+//            answer = answer.concat(Integer.toString(temp,n));
+//            ++temp;
+//        }
+//
+//        StringBuilder sb = new StringBuilder();
+//        for(int i=p-1; i<=(t-1)*m+p; i+=m ) {
+//            sb.append(answer.charAt(i));
+//        }
+//
+//        answer = sb.toString().toUpperCase();
+//
+//        return answer;
+
+
+
+        //1차 풀이
         String answer = "";
         int number = 0;
         int index = 1;
